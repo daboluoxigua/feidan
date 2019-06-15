da3ad866c70e7c6c626ac55dfba96e1c574cc03d
daboluoxigua <369632586@qq.com>
2019-06-15 20:00:02 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,125 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-row>
+      <el-col :span="4">
+        <div class="title">
+          <i class="el-icon-setting"></i>
+          <p>同步设置</p>
+        </div>
+      </el-col>
+      <el-col :span="20">
+        <el-form :model="synchronization" label-width="80px">
+          <el-form-item label="房间编号">
+            <el-input v-model="synchronization.number"></el-input>
+          </el-form-item>
+          <el-form-item label="打盘游戏">
+            <el-checkbox
+              v-for="(item,index) in synchronization.game"
+              :key="index"
+              :label="item.name"
+              v-model="item.checked"
+            ></el-checkbox>
+          </el-form-item>
+          <el-form-item label="语音提示">
+            <el-checkbox
+              v-for="(item,index) in synchronization.voice"
+              :key="index"
+              :label="item.name"
+              v-model="item.checked"
+            ></el-checkbox>
+          </el-form-item>
+          <el-form-item label="两面对冲">
+            <el-checkbox label="开启两面对冲" v-model="synchronization.hedging"></el-checkbox>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <div class="title site">
+          <i class="el-icon-guide"></i>
+          <p>站点1</p>
+        </div>
+      </el-col>
+      <el-col :span="20">
+        <el-form :model="site1" label-width="80px">
+          <el-form-item label="站点类型">
+            <el-checkbox v-for="(item,index) in site1.type" :key="index" :label="item.name" v-model="item.checked"></el-checkbox>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <div class="title site">
+          <i class="el-icon-guide"></i>
+          <p>站点2</p>
+        </div>
+      </el-col>
+      <el-col :span="20">2</el-col>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <div class="title site">
+          <i class="el-icon-guide"></i>
+          <p>站点3</p>
+        </div>
+      </el-col>
+      <el-col :span="20">2</el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      synchronization: {
+        number: "",
+        game: [
+          { name: "北京赛车", gameId: 1, checked: false },
+          { name: "幸运废土", gameId: 2, checked: true }
+        ],
+        voice: [
+          { name: "北京赛车", gameId: 1, checked: false },
+          { name: "幸运废土", gameId: 2, checked: true }
+        ]
+      },
+      site1: {
+        type: [
+          { name: "IDC", ID: 1, checked: false },
+          { name: "SGWIN", ID: 1, checked: false },
+          { name: "MOA", ID: 1, checked: false }
+        ]
+      }
+    };
+  },
+};
 </script>
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
+#app > .el-row {
+  margin-top: 15px;
+}
+.title {
+  padding: 25px;
+  text-align: center;
+  background: #F56C6C;
+  color: #fff;
+}
+.title.site {
+  background: #67C23A;
+}
+.title i {
+  font-size: 40px;
+}
+.title p {
+  margin-top: 10px;
+}
+
 </style>
